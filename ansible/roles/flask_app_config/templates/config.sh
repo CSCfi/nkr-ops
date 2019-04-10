fca0ae8079b120054a3e88f4885baae9c3096a1a
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2019-04-10 14:49:24 +0300

    Add config files to flask app and make gunicorn use them

---
+++ ansible/roles/flask_app_config/templates/config.sh
@@ -0,0 +1,10 @@
+DEBUG={{ flask_app.debug }}
+LOG_LEVEL={{ flask_app.log_level }}
+
+REMS_URL={{ flask_app.rems_url }}
+REMS_API_KEY={{ flask_app.rems_api_key }}
+
+INDEX_URL={{ flask_app.index_url }}
+INDEX_NAME={{ flask_app.index_name }}
+INDEX_USERNAME={{ flask_app.index_username }}
+INDEX_PASSWORD={{ flask_app.index_password }}
--- ansible/roles/gunicorn/templates/gunicorn_conf.py
+++ ansible/roles/gunicorn/templates/gunicorn_conf.py
@@ -1,17 +1,19 @@
 """
 gunicorn WSGI server configuration.
-
 """
+
 from multiprocessing import cpu_count
-from os import environ
+
 
 def max_workers():
     return cpu_count() * 2 + 1
 
+
 max_requests = 100
 worker_class = 'gevent'
 workers = max_workers()
-timeout = 259200 # 3 days
+timeout = 60
+
 
 secure_scheme_headers = {
     'X-FORWARDED-PROTOCOL': 'ssl',
