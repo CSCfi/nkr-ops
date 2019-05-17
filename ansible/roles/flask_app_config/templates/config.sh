74705a40192f2135eb7fd5fcae684eb695582cc9
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2019-05-17 13:37:28 +0300

    Update config.sh template, fix typo in proxyservers group_vars

--- ansible/roles/flask_app_config/templates/config.sh
+++ ansible/roles/flask_app_config/templates/config.sh
@@ -1,12 +1,12 @@
 DEBUG={{ flask_app.debug }}
-LOG_LEVEL={{ flask_app.log_level }}
+LOG_LEVEL="{{ flask_app.log_level }}"
 
-REMS_URL={{ flask_app.rems_url }}
-REMS_API_KEY={{ flask_app.rems_api_key }}
+REMS_URL="{{ flask_app.rems_url }}"
+REMS_API_KEY="{{ flask_app.rems_api_key }}"
 
-INDEX_URL={{ flask_app.index_url }}
-INDEX_NAME={{ flask_app.index_name }}
-INDEX_USERNAME={{ flask_app.index_username }}
-INDEX_PASSWORD={{ flask_app.index_password }}
+INDEX_URL="{{ flask_app.index_url }}"
+INDEX_NAME="{{ flask_app.index_name }}"
+INDEX_USERNAME="{{ flask_app.index_username }}"
+INDEX_PASSWORD="{{ flask_app.index_password }}"
 
-LEVEL_RESTRICTION_FIELD={{ flask_app.level_restriction_field }}
+LEVEL_RESTRICTION_FIELD="{{ flask_app.level_restriction_field }}"
