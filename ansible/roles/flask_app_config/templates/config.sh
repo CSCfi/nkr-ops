cf7c4f03c6249ac39b0eb8a0d0e2f19f1b576cf1
Hannu Kamarainen <hannu.kamarainen@csc.fi>
2019-10-03 12:05:36 +0300

    CSCNKR-80: [ADD] Add support for using ip-list when connecting to index. Add better documentation to config.sh template file

--- ansible/roles/flask_app_config/templates/config.sh
+++ ansible/roles/flask_app_config/templates/config.sh
@@ -1,17 +1,60 @@
+
+### general configurations
+
+
+# when value is local_development, app takes into account some small differences in dev env
 NKR_ENV="{{ deployment_environment_id }}"
-DEBUG="{{ flask_app.debug }}"
-LOG_LEVEL="{{ flask_app.log_level }}"
+
+# extra debugging info and diagnostics output
+DEBUG="{{ flask_app.debug | default('0') }}"
+
+# extra debugging info and diagnostics output. this is passed to python logging module.
+LOG_LEVEL="{{ flask_app.log_level | default('INFO') }}"
+
+# if envs use self-signed certificates, set to false
+VERIFY_TLS="{{ flask_app.verify_tls | default('1') }}"
+
+
+### rems configurations
+
 
 REMS_URL="{{ flask_app.rems_url }}"
 REMS_API_KEY="{{ flask_app.rems_api_key }}"
 
-INDEX_URL="{{ flask_app.index_url }}"
+
+### index configurations
+
+
+# from the below four, either of the following must be defined:
+# - INDEX_IP_LIST       # comma-separated list of ip-addresses
+# - INDEX_MAIN_API      # main api where index service is reached, e.g. "/index"
+# - INDEX_HOSTNAME      # the value that is set to the HTTP header "Host"
+# OR
+# - INDEX_URL           # should contain full url to index api, including http protocol, hostname,
+#                       # and api, e.g. https://myindex.fi/index or http://localhost:port/index
+
+INDEX_URL="{{ flask_app.index_url | default('') }}"
+INDEX_IP_LIST="{{ flask_app.index_ip_list | default('') }}"
+INDEX_MAIN_API="{{ flask_app.index_main_api | default('') }}"
+INDEX_HOSTNAME="{{ flask_app.index_hostname | default('') }}"
+
 INDEX_NAME="{{ flask_app.index_name }}"
+
+# credentials for accessing the index
 INDEX_USERNAME="{{ flask_app.index_username }}"
 INDEX_PASSWORD="{{ flask_app.index_password }}"
+
+# comma-separated list of apis of the index that can be used through the proxy
 INDEX_ALLOWED_APIS="{{ flask_app.index_allowed_apis }}"
 
+# name of the field in an index document that defines the document's access restriction level
 LEVEL_RESTRICTION_FIELD="{{ flask_app.level_restriction_field }}"
+
+# name of the unique id field in an index document. "id" field is not actually unique,
+# since multiple documents of different restriction levels can have the same id.
+# from the point of view of a requesting service, the "id" field IS unique, since only
+# one level of a document can be returned at a time.
 DOCUMENT_UNIQUE_ID_FIELD="{{ flask_app.document_unique_id_field }}"
 
+# identifier of the resource in rems that grants access to level 10 metadata
 METADATA_LEVEL_10_RESOURCE_ID="{{ flask_app.metadata_level_10_resource_id }}"
